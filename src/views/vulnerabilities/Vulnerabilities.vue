82b75f2eb49ee9f151a5e9b2d18954c8c42a387b
Eric Pitcock <accounts+git@ericpitcock.com>
2024-03-28 17:00:32 -0700

    vulns updates: only show high and critical, progress updates

--- src/views/vulnerabilities/Vulnerabilities.vue
+++ src/views/vulnerabilities/Vulnerabilities.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="vulnerabilities">
+    <ep-header v-bind="headerProps">
+      <template #left>
+        <p>Vulnerabilities</p>
+      </template>
+      <template #right>
+        <ep-date-picker
+          mode="range"
+          positionX="right"
+          :inputProps="{
+            size: 'large',
+            backgroundColor: 'var(--interface-foreground)'
+          }"
+        />
+      </template>
+    </ep-header>
     <ep-container
       v-bind="commonContainerProps"
       content-padding="3rem 0 1rem 0"
@@ -7,17 +22,17 @@
       <template #header>
         <ep-header v-bind="commonHeaderProps">
           <template #left>
-            <h1>Vulnerabilities Over Time</h1>
+            <h1>Severity Over Time</h1>
           </template>
           <template #right>
-            <ep-date-picker
+            <!-- <ep-date-picker
               mode="range"
               positionX="right"
               :inputProps="{
                 size: 'large',
                 backgroundColor: 'var(--interface-foreground)'
               }"
-            />
+            /> -->
           </template>
         </ep-header>
       </template>
@@ -50,6 +65,16 @@
     },
     data() {
       return {
+        headerProps: {
+          backgroundColor: 'var(--interface-surface)',
+          leftFlex: '0 0 10rem',
+          centerFlex: '1',
+          rightFlex: '0 0 10rem',
+          sticky: true,
+          stickyTop: '0',
+          padding: '0 3rem',
+          zIndex: 'var(--z-index--fixed)',
+        },
         vulnChartOptions,
         vulnData: vulnData.default,
         tableColumns: [
@@ -107,8 +132,8 @@
 
 <style lang="scss" scoped>
   .vulnerabilities {
-    padding: 3rem;
 
+    // padding: 3rem;
     > *:not(:first-child) {
       margin-top: 2rem;
     }
