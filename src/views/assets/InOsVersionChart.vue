05b878d18270a6d4c7ce558105c316b01567e07b
Eric Pitcock <accounts+git@ericpitcock.com>
2024-04-14 13:26:19 -0700

    prog

--- src/views/assets/InOsVersionChart.vue
+++ src/views/assets/InOsVersionChart.vue
@@ -9,162 +9,63 @@
 
 <script setup>
   import Highcharts from 'highcharts'
-  // import { windowsVersions, macOSVersions, linuxDistributions } from '@/utils/helpers.js'
+  import { windowsVersions, macOSVersions, linuxDistributions } from '@/utils/helpers.js'
 
   const colors = Highcharts.getOptions().colors
 
   const categories = [
-    'Chrome',
-    'Safari',
-    'Edge',
-    'Firefox',
-    'Other'
+    'Windows',
+    'macOS',
+    'Linux',
+    // 'Firefox',
+    // 'Other'
   ],
     data = [
       {
-        y: 61.04,
+        y: 60,
         color: colors[2],
         drilldown: {
-          name: 'Chrome',
-          categories: [
-            'Chrome v97.0',
-            'Chrome v96.0',
-            'Chrome v95.0',
-            'Chrome v94.0',
-            'Chrome v93.0',
-            'Chrome v92.0',
-            'Chrome v91.0',
-            'Chrome v90.0',
-            'Chrome v89.0',
-            'Chrome v88.0',
-            'Chrome v87.0',
-            'Chrome v86.0',
-            'Chrome v85.0',
-            'Chrome v84.0',
-            'Chrome v83.0',
-            'Chrome v81.0',
-            'Chrome v89.0',
-            'Chrome v79.0',
-            'Chrome v78.0',
-            'Chrome v76.0',
-            'Chrome v75.0',
-            'Chrome v72.0',
-            'Chrome v70.0',
-            'Chrome v69.0',
-            'Chrome v56.0',
-            'Chrome v49.0'
-          ],
+          name: 'Windows',
+          categories: windowsVersions,
           data: [
-            36.89,
-            18.16,
-            0.54,
-            0.7,
-            0.8,
-            0.41,
-            0.31,
-            0.13,
-            0.14,
-            0.1,
-            0.35,
-            0.17,
-            0.18,
-            0.17,
-            0.21,
-            0.1,
-            0.16,
-            0.43,
-            0.11,
-            0.16,
-            0.15,
-            0.14,
-            0.11,
-            0.13,
-            0.12
+            25,
+            15,
+            10,
+            5,
+            5,
           ]
         }
       },
       {
-        y: 9.47,
+        y: 30,
         color: colors[3],
         drilldown: {
-          name: 'Safari',
-          categories: [
-            'Safari v15.3',
-            'Safari v15.2',
-            'Safari v15.1',
-            'Safari v15.0',
-            'Safari v14.1',
-            'Safari v14.0',
-            'Safari v13.1',
-            'Safari v13.0',
-            'Safari v12.1'
-          ],
+          name: 'macOS',
+          categories: macOSVersions,
           data: [
-            0.1,
-            2.01,
-            2.29,
-            0.49,
-            2.48,
-            0.64,
-            1.17,
-            0.13,
-            0.16
+            16,
+            5,
+            4,
+            3,
+            2,
           ]
         }
       },
       {
-        y: 9.32,
+        y: 10,
         color: colors[5],
         drilldown: {
-          name: 'Edge',
-          categories: [
-            'Edge v97',
-            'Edge v96',
-            'Edge v95'
-          ],
+          name: 'Linux',
+          categories: linuxDistributions,
           data: [
-            6.62,
-            2.55,
-            0.15
+            5,
+            2,
+            1,
+            1,
+            1,
           ]
         }
       },
-      {
-        y: 8.15,
-        color: colors[1],
-        drilldown: {
-          name: 'Firefox',
-          categories: [
-            'Firefox v96.0',
-            'Firefox v95.0',
-            'Firefox v94.0',
-            'Firefox v91.0',
-            'Firefox v78.0',
-            'Firefox v52.0'
-          ],
-          data: [
-            4.17,
-            3.33,
-            0.11,
-            0.23,
-            0.16,
-            0.15
-          ]
-        }
-      },
-      {
-        y: 11.02,
-        color: colors[6],
-        drilldown: {
-          name: 'Other',
-          categories: [
-            'Other'
-          ],
-          data: [
-            11.02
-          ]
-        }
-      }
     ],
     browserData = [],
     versionsData = [],
@@ -174,7 +75,6 @@
     j,
     drillDataLen,
     brightness
-
 
   // Build the data arrays
   for (i = 0; i < dataLen; i += 1) {
@@ -208,14 +108,6 @@
       type: 'pie',
       styledMode: true
     },
-    title: {
-      text: 'Browser market share, January, 2022',
-      align: 'left'
-    },
-    subtitle: {
-      text: 'Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>',
-      align: 'left'
-    },
     plotOptions: {
       pie: {
         shadow: false,
@@ -226,7 +118,7 @@
       valueSuffix: '%'
     },
     series: [{
-      name: 'Browsers',
+      name: 'Operating Systems',
       data: browserData,
       size: '45%',
       dataLabels: {
@@ -239,16 +131,19 @@
       size: '80%',
       innerSize: '60%',
       dataLabels: {
-        format: '<b>{point.name}:</b> <span style="opacity: 0.5">{y}%</span>',
-        filter: {
-          property: 'y',
-          operator: '>',
-          value: 1
-        },
-        style: {
-          fontWeight: 'normal'
-        }
+        enabled: false // Remove data labels
       },
+      // dataLabels: {
+      //   format: '<b>{point.name}:</b> <span style="opacity: 0.5">{y}%</span>',
+      //   filter: {
+      //     property: 'y',
+      //     operator: '>',
+      //     value: 1
+      //   },
+      //   style: {
+      //     fontWeight: 'normal'
+      //   }
+      // },
       id: 'versions'
     }],
     responsive: {
